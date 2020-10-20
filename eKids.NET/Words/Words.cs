4f7e65db35423c36c2f37497ef588ff9d655e64c
Ivan Kirkorau <srcerror@tut.by>
2020-10-20 19:00:50 +0300

    initial anagramma code

--- eKids.NET/Words/Words.cs
+++ eKids.NET/Words/Words.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,6 +9,20 @@ namespace eKids
 {
     public static class WordsExtensions
     {
+        public static IEnumerable<string> FindWords(this TextReader file, bool toLower = false)
+        {
+            var line = file.ReadLine();
+            while (line != null)
+            {
+                foreach(var word in line.FindWords(toLower))
+                {
+                    yield return word;
+                }
+
+                line = file.ReadLine();
+            }
+        }
+
         public static IEnumerable<string> FindWords(this IEnumerable<char> input, bool toLower = false)
         {
             var sb = new StringBuilder();
@@ -63,6 +78,7 @@ namespace eKids
                 yield return other;
             }
         }
+        
         public static IEnumerable<string> FindTokens(this IEnumerable<char> input, bool toLower = false)
         {
             var sw = new StringBuilder();
@@ -132,6 +148,17 @@ namespace eKids
                 if (toLower) word = word.ToLower();
                 yield return word;
             }
+        }
+
+        public static string AsString(this IEnumerable<char> input)
+        {
+            return new string(input.ToArray());
+        }
+
+        public static string ToInvariantKey(this string input)
+        {
+            var clean = input.Where(x => char.IsLetterOrDigit(x));
+            return clean.OrderBy(x => x).AsString();
         }
     }
 }
