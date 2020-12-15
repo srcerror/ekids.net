2b541cdf6fbd7746bd1e9ec7c84ebe69ceb48c33
Ivan Kirkorau <srcerror@tut.by>
2020-12-15 19:15:08 +0300

    Initial bot solution

---
+++ eKids.NET/DarkCastleBot/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DarkCastleBot
+{
+    public static class Configuration
+    {
+        public readonly static string BotToken = "1458903805:AAGHWKlxrudPlNjBgco8jx-YjVXqxMr8nLk";
+
+#if USE_PROXY
+        public static class Proxy
+        {
+            public readonly static string Host = "{PROXY_ADDRESS}";
+            public readonly static int Port = 8080;
+        }
+#endif
+    }
+}
